29f5816646fda61205d2093ae8127d820f75c35d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-25 00:25:51 +0000

    Deploy iandtek/COMP3104 to github.com/iandtek/COMP3104.git:gh-pages

---
+++ precache-manifest.e56d8cc835da671d7ab2cf5eceb20afe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5169bdee5cf6a0249804",
+    "url": "/iandtek/COMP3401/static/css/main.21b186f9.chunk.css"
+  },
+  {
+    "revision": "5169bdee5cf6a0249804",
+    "url": "/iandtek/COMP3401/static/js/main.5169bdee.chunk.js"
+  },
+  {
+    "revision": "64b07fd5145e93a8b1d2",
+    "url": "/iandtek/COMP3401/static/js/runtime~main.64b07fd5.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/iandtek/COMP3401/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "3a7b8b7e17b3fd749bf7d7ef776096bf",
+    "url": "/iandtek/COMP3401/index.html"
+  }
+];
